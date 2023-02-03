e39133cf672be60f8c9fd50076c80c074052a6ea
YaredT21 <teklewold.yared21@gmail.com>
2023-02-03 04:55:24 +0300

    completed task

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,78 +1,43 @@
 #!/usr/bin/python3
-'''N Queens Challenge'''
-import sys
+'''
+Module that solves the N Queens puzzle
+'''
+from sys import argv, exit
 
-
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print('Usage: nqueens N')
+        exit(1)
     try:
-        n = int(sys.argv[1])
-    except ValueError:
+        n = int(argv[1])
+    except BaseException:
         print('N must be a number')
         exit(1)
     if n < 4:
         print('N must be at least 4')
         exit(1)
 
-    all_qs = []
-    queens = []
-    stop = False
-    r = 0
-    c = 0
-    while r < n:
-        goback = False
-        while c < n:
-            safe = True
-            for qn in queens:
-                col = qn[1]
-                if(col == c or col + (r - qn[0]) == c or
-                        col - (r - qn[0]) == c):
-                    safe = False
-                    break
-            if not safe:
-                if c == n - 1:
-                    goback = True
-                    break
-                c += 1
-                continue
-            qns = [r, c]
-            queens.append(qns)
-            if r == n - 1:
-                all_qs.append(queens[:])
-                for qn in queens:
-                    if qn[1] < n - 1:
-                        r = qn[0]
-                        c = qn[1]
-                for i in range(n - r):
-                    queens.pop()
-                if r == n - 1 and c == n - 1:
-                    queens = []
-                    stop = True
-                r -= 1
-                c += 1
-            else:
-                c = 0
-            break
-        if stop:
-            break
-        if goback:
-            r -= 1
-            while r >= 0:
-                c = queens[r][1] + 1
-                del queens[r]
-                if c < n:
-                    break
-                r -= 1
-            if r < 0:
-                break
-            continue
-        r += 1
+    solution = []
 
-    for idx, val in enumerate(all_qs):
-        if idx == len(all_qs) - 1:
-            print(val, end='')
+    def solve_queens(row, n, solution):
+        if (row == n):
+            print(solution)
         else:
-            print(val)
-    print()
+            for col in range(n):
+                placement = [row, col]
+                if valid_placement(solution, placement):
+                    solution.append(placement)
+                    solve_queens(row + 1, n, solution)
+                    solution.remove(placement)
+
+    def valid_placement(solution, placement):
+        for queen in solution:
+            if queen[1] == placement[1]:
+                return False
+            if (queen[0] + queen[1]) == (placement[0] + placement[1]):
+                return False
+            if (queen[0] - queen[1]) == (placement[0] - placement[1]):
+                return False
+        return True
+
+    solve_queens(0, n, solution)
